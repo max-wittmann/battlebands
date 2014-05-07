474194e87e0ab57f35755f31a620460111270478
max-wittmann <damxam@gmail.com>
2014-05-08 09:32:14 +1000

    Move server-starting js to root

---
+++ index.js
@@ -0,0 +1,5 @@
+var server = require("./src/server/server")
+
+console.log("Starting...")
+server.start();
+console.log("Listening...")
--- src/server/server.js
+++ src/server/server.js
@@ -1,32 +1,41 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
+// var app = require('http').createServer(handler)
+//   , io = require('socket.io').listen(app)
+//   , fs = require('fs')
+
+var io = require('socket.io')
   , fs = require('fs')
+  , http = require('http');
 
-console.log("Starting...")
-app.listen(80);
-console.log("Listening...")
+function start() {
+  function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+      res.writeHead(200);
+      res.end(data);
+    });
+  }
 
-    res.writeHead(200);
-    res.end(data);
+  var server = http.createServer(handler).listen(8080);
+  io = io.listen(server)
+
+  io.sockets.on('connection', function (socket) {
+      socket.emit('news', { hello: 'world' });
+      socket.on('my other event', function (data) {
+            console.log("Other data: " + data);
+        });
+      socket.on('clapping', function(data) {
+        console.log("Clap Clap: " + JSON.stringify(data) + ", " + data.thing)
+        console.log(new Date().getTime() + " : " + data.time)
+        console.log("Time taken: " + ((new Date()).getTime() - data.time))
+      });
   });
+
+  // server.listen(80);
 }
 
-io.sockets.on('connection', function (socket) {
-	  socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-	        console.log("Other data: " + data);
-		  });
-    socket.on('clapping', function(data) {
-      console.log("Clap Clap: " + JSON.stringify(data) + ", " + data.thing)
-      console.log(new Date().getTime() + " : " + data.time)
-      console.log("Time taken: " + ((new Date()).getTime() - data.time))
-    });
-});
+exports.start = start;
